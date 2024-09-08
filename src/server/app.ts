fc9169272890699ae19d06a835b168118b5d9472
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-08 23:24:34 +0200

    Create test for general error

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,10 +3,6 @@ import { generalError } from "./error/generalError/generalError.js";
 
 const app = express();
 
-app.use((_req, res) => {
-  res.status(404).json({ message: "Path not found" });
-});
-
 app.use(generalError);
 
 export default app;
