92f67e5b364fcb53d93714247c51edc6236e96b1
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-08 22:12:59 +0200

    Create generalError

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import { generalError } from "./error/generalError/generalError.js";
 
 const app = express();
+
+app.use((_req, res) => {
+  res.status(404).json({ message: "Path not found" });
+});
+
+app.use(generalError);
 
 export default app;
---
+++ src/server/error/generalError/generalError.ts
@@ -0,0 +1,13 @@
+import type { NextFunction, Request, Response } from "express";
+import type { ServerError } from "../ServerError.js";
+
+export const generalError = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): void => {
+  const statusCode = (error as ServerError).statusCode ?? 500;
+
+  res.status(statusCode).json({ message: error.message });
+};
