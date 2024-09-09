5a26e7dd80f9c32123786f49b553c8d882e0f161
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-09 18:10:27 +0200

    Create healthCheck

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 import { generalError } from "./error/generalError/generalError.js";
 import notFoundError from "./error/notFoundError/notFoundError.js";
+import healthCheck from "./healthCheck/healthCheck.js";
 
 const app = express();
+
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+
+app.get("/", healthCheck);
 
 app.use(notFoundError);
 
---
+++ src/server/healthCheck/healthCheck.ts
@@ -0,0 +1,7 @@
+import { type Request, type Response } from "express";
+
+const healthCheck = (_req: Request, res: Response) => {
+  res.status(200).json({ message: "OK!" });
+};
+
+export default healthCheck;
