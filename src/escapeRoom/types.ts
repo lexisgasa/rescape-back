c7cf0ebdb6d7450d08b7ac05254927f3ada621d9
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-09-12 15:11:49 +0200

    Modify EscapeRoomStructure

--- src/escapeRoom/types.ts
+++ src/escapeRoom/types.ts
@@ -1,5 +1,8 @@
 export interface EscapeRoomStructure {
+  id: string;
   name: string;
+  location: string;
+  date: Date;
   alternativeText: string;
   smallImageUrl: string;
   detailImageUrl: string;
